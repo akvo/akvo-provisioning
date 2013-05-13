c949860d4814931b09ee984ecfe08c2259d0586e
carlio <git@carlcrowder.com>
2013-05-13 13:21:47 +0200

    Adding some config for the "opstest" environment

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -16,6 +16,10 @@ case $::environment {
         $management_server_ip = '127.0.0.1'
         $base_domain = 'localdev.akvo.org'
     }
+    'opstest' {
+        $management_server_ip = '72.44.50.191'
+        $base_comain = 'opstest.akvo.org'
+    }
     'live': {
         $base_domain = 'akvo.org'
     }
