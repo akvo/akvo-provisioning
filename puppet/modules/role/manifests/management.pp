9ad1c1c16b16798fe4512654926ae61113330808
carlio <git@carlcrowder.com>
2013-05-16 17:35:06 +0200

    Directly adding the management server config

--- puppet/modules/role/manifests/management.pp
+++ puppet/modules/role/manifests/management.pp
@@ -3,7 +3,7 @@ class role::management {
     notice("Including role: management")
 
     # let the DNS server know where we are
-    @@bind::service_location { "management":
+    bind::service_location { "management":
         ip => hiera('external_ip')
     }
 
