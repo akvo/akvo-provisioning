99742d5054f64c2f3615be54f6a0ae277fdda37a
carlio <git@carlcrowder.com>
2013-05-17 12:15:08 +0200

    More noodling around with variables; it is not clear which bits are stored in stored config...

--- puppet/modules/munin/manifests/nodeinfo.pp
+++ puppet/modules/munin/manifests/nodeinfo.pp
@@ -1,7 +1,7 @@
 
 define munin::nodeinfo( $target_fqdn ) {
 
-    file_line { $name:
+    file_line { 'munin-node-${target_fqdn}':
         path   => '/etc/munin/munin.conf',
         line   => inline_template("[<%= target_fqdn %>]\n  address <%= target_fqdn %>"),
         tag    => 'munin-node-host',
