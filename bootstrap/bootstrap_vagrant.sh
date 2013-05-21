413ec9c849166e77ee707b084166f4bb55e46f05
carlio <git@carlcrowder.com>
2013-05-21 15:22:00 +0200

    Setting the nameserver in resolv.conf as it cannot be automatically delegated from akvo.org (this is only necessary for local vagrant boxes)

--- bootstrap/bootstrap_vagrant.sh
+++ bootstrap/bootstrap_vagrant.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+function set_nameserver {
+    sed -i '/nameserver/d' /etc/resolv.conf
+    echo 'nameserver 192.168.50.101' >> /etc/resolv.conf
+}
+
+
 if [ -e /etc/akvo_provisioned ]
 then
     echo "Already bootstrapped, so nothing to do"
+    set_nameserver
     exit 0
 fi
 
@@ -13,8 +20,10 @@ sudo apt-get install -y -q fabric
 
 orig_dir=`pwd`
 cd /vagrant/bootstrap/
-./bootstrap.sh $extra_args -A "--linewise --password=vagrant" -u vagrant localhost localdev $roles
+./bootstrap.sh -A "--linewise --password=vagrant" -u vagrant localhost localdev $roles
 cd $orig_dir
 
 
 echo `date` > /etc/akvo_provisioned
+
+set_nameserver
