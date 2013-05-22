df7ec917df28830c919f3a10b4d71f79c5a2d78f
carlio <git@carlcrowder.com>
2013-05-22 17:16:40 +0200

    Adding in a psql helper module, and creating the local configuration required to connect to the database

--- puppet/modules/akvoapp/manifests/pythonsupport/psql.pp
+++ puppet/modules/akvoapp/manifests/pythonsupport/psql.pp
@@ -1,7 +1,7 @@
 # includes the system packages needed to talk to a postgres database
 # server from python
 
-class akvoapp::pythonsupport::psqlclient {
+class akvoapp::pythonsupport::psql {
 
   $required_packages = ['libpq-dev']
 
--- puppet/modules/rsr/manifests/common.pp
+++ puppet/modules/rsr/manifests/common.pp
@@ -2,14 +2,26 @@
 # and development versions of RSR
 
 class rsr::common {
+
+    # some shared config
+    $database_password = 'lake'
+    $base_domain = hiera('base_domain')
+    $database_host = "psql.${base_domain}"
+    $media_root = "/apps/rsr/checkout/akvo/mediaroot/"
+    $logdir = "/tmp/logs"
+
+
     # make sure we also include the Akvoapp stuff, and that it is loaded
     # before this module
     require akvoapp
+
 
     # include our RSR-specific akvo info
     akvoapp::app { 'rsr': } # no parameters yet
     akvoapp::djangoapp { 'rsr': }
 
+
+    # install all of the support packages
     # currently, use of mysql is hardcoded in the requirements file
     # this should be removed
     include akvoapp::pythonsupport::mysql
@@ -18,9 +30,20 @@ class rsr::common {
     include akvoapp::pythonsupport::pil
     include akvoapp::pythonsupport::lxml
 
+
     # create an RSR database on the database server
     @@database::psql::db { 'rsr':
-        password => 'lake'
+        password => $database_password
+    }
+
+
+    # add custom configuration
+    file { '/apps/rsr/checkout/akvo/settings/60_puppet.conf':
+        ensure   => present,
+        owner    => 'rsr',
+        group    => 'rsr',
+        mode     => 444,
+        content  => template('rsr/local.conf.erb'),
     }
 
 }
