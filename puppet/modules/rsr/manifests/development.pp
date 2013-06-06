8a335c8001880853ec69e1ddc28e40369396a359
carlio <git@carlcrowder.com>
2013-06-06 19:40:05 +0200

    Removing use of django option which does not exist in 1.4

--- puppet/modules/rsr/manifests/development.pp
+++ puppet/modules/rsr/manifests/development.pp
@@ -17,7 +17,7 @@ class rsr::development {
 
     # run the database setup and syncronisation
     rsr::djangocommand { "syncdb":
-        command  => 'syncdb --noinput --no-initial-data'
+        command  => 'syncdb --noinput'
     }
     rsr::djangocommand { "migrate":
         require => Rsr::Djangocommand['syncdb'],
