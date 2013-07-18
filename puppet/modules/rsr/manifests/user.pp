973a06423db4cfe48c6d977497d0fe3980825483
Carl Crowder <carl@akvo.org>
2013-07-18 13:47:57 +0200

    Using the deploy key for the RSR user

--- puppet/modules/rsr/manifests/user.pp
+++ puppet/modules/rsr/manifests/user.pp
@@ -12,4 +12,20 @@ define rsr::user ( $username, $approot ) {
         require => User[$username],
     }
 
+    file { "${approot}/.ssh":
+        ensure  => directory,
+        owner   => $username,
+        group   => $username,
+        mode    => 700,
+        require => File[$approot],
+    }
+
+    ssh_authorized_key { "rsr-deploy_key":
+        ensure  => present,
+        key     => hiera('rsr-deploy_public_key'),
+        type    => 'ssh-rsa',
+        user    => 'rsr',
+        require => File["${approot}/.ssh"]
+    }
+
 }
