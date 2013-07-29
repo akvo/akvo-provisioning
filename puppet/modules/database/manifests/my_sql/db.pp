d67d4e23979e2e39afc639177b828956bd90dd31
Carl Crowder <carl@akvo.org>
2013-07-29 13:57:46 +0200

    Fixing syntax error in mysql manifest

--- puppet/modules/database/manifests/my_sql/db.pp
+++ puppet/modules/database/manifests/my_sql/db.pp
@@ -1,7 +1,7 @@
 define database::my_sql::db ( $password ) {
 
     @@database::my_sql::db_exported{ $name:
-        password => $password
+        password => $password,
         tag      => $::environment
     }
 
