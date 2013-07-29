75c0ca5712cd4452720057d28593203e1f3bc91b
Carl Crowder <carl@akvo.org>
2013-07-29 14:35:17 +0200

    the management service is not referred to by name nad trying to export it causes circular problems

--- puppet/modules/role/manifests/management.pp
+++ puppet/modules/role/manifests/management.pp
@@ -1,11 +1,5 @@
 class role::management {
 
     notice("Including role: management")
-
-    # let the DNS server know where we are
-    named::service_location { "management":
-        ip => hiera('external_ip')
-    }
-
     include named
 }
