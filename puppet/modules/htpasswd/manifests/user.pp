a857e640344356083a183c5cfaae27908e22a19b
Carl Crowder <carl@akvo.org>
2013-07-30 14:38:52 +0200

    Ensuring empty file is present in order to add lines to it

--- puppet/modules/htpasswd/manifests/user.pp
+++ puppet/modules/htpasswd/manifests/user.pp
@@ -3,6 +3,14 @@ define htpasswd::user ($user, $role, $password) {
 
     include htpasswd::fs
 
+    ensure_resource('file', "/etc/htpasswd/${role}", {
+        ensure  => present,
+        owner   => root,
+        group   => root,
+        mode    => 444,
+        require => File['/etc/htpasswd']
+    })
+
     file_line { "htpasswd-${user}-${role}":
         path    => "/etc/htpasswd/${role}",
         line    => inline_template("${user}:${password}"),
