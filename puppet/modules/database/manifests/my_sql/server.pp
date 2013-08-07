7f96e888bcbc5bbe7c76473d46d42d1b6369b60f
Carl Crowder <carl@akvo.org>
2013-08-07 19:45:16 +0200

    Pulling the mysql root password out of hiera config

--- puppet/modules/database/manifests/my_sql/server.pp
+++ puppet/modules/database/manifests/my_sql/server.pp
@@ -4,7 +4,7 @@ class database::my_sql::server {
         # for possible values in this hash, see
         # https://github.com/puppetlabs/puppetlabs-mysql/blob/master/manifests/config.pp#L5
         config_hash => {
-            'root_password'  => 'foo',  # TODO: better password :)
+            'root_password'  => hiera('mysql_root_password'),
             'bind_address'   => hiera('external_ip'),
             'character_set'  => 'utf8',
         }
