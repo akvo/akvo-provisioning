6c4aea00793e976a8901e8d851e9bda27ad8d517
Carl Crowder <carl@akvo.org>
2013-08-08 10:32:31 +0200

    Adding media_root parameter for rsr static content

--- puppet/modules/rsr/manifests/staticcontent.pp
+++ puppet/modules/rsr/manifests/staticcontent.pp
@@ -1,4 +1,5 @@
-define rsr::staticcontent {
+define rsr::staticcontent ($media_root) {
+
     file { "${media_root}/${name}":
         ensure  => 'link',
         target  => "${approot}/code/akvo/mediaroot/${name}",
