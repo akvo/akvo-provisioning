93062656501e62c61442fa04f33e1eb03f6dbbb6
Carl Crowder <carl@akvo.org>
2013-08-12 18:03:39 +0200

    Ensuring the supervisord package is included if a service is created

--- puppet/modules/supervisord/manifests/service.pp
+++ puppet/modules/supervisord/manifests/service.pp
@@ -1,13 +1,14 @@
 define supervisord::service (
    $user, $command, $directory = undef, $env_vars = undef ) {
 
-   include supervisord::update
+    include supervisord
+    include supervisord::update
 
-   file { "/etc/supervisor/conf.d/${name}.conf":
-       ensure  => 'present',
-       content => template("supervisord/program.conf"),
-       require => Package['supervisor'],
-       notify  => Class['Supervisord::Update'],
-   }
+    file { "/etc/supervisor/conf.d/${name}.conf":
+        ensure  => 'present',
+        content => template("supervisord/program.conf"),
+        require => Package['supervisor'],
+        notify  => Class['Supervisord::Update'],
+    }
 
 }
