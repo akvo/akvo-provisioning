24e037af42529b0f80099001b11c2142975144ee
Carl Crowder <carl@akvo.org>
2013-08-12 11:25:05 +0200

    Initial module for self-hosted pypi [refs #12]

---
+++ puppet/modules/pypi/manifests/server.pp
@@ -0,0 +1,42 @@
+class pypi::server {
+
+    package { 'devpi':
+        ensure   => 'installed',
+        provider => 'pip'
+    }
+
+    user { 'devpi':
+        ensure => present,
+        home   => '/srv/devpi',
+        shell  => '/bin/bash',
+    }
+
+    file { '/srv/devpi':
+        ensure  => directory,
+        owner   => 'devpi',
+        mode    => 744,
+        require => User['devpi']
+    }
+
+    supervisord::service { 'devpi':
+        user    => 'devpi',
+        command => "/usr/local/bin/devpi-server --datadir=/srv/devpi",
+        require => File['/srv/devpi'],
+    }
+
+    sudo::service_control { 'devpi':
+        user      => 'devpi',
+    }
+
+    $base_domain = hiera('base_domain')
+    nginx::proxy { "pypi.${base_domain}":
+        proxy_url        => 'http://localhost:3141',
+        ssl              => false,
+        htpasswd_source  => 'puppet:///modules/pypi/htpasswd'
+    }
+
+    named::service_location { 'pypi':
+        ip => hiera('external_ip')
+    }
+
+}
