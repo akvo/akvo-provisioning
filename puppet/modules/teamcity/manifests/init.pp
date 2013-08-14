8b3e21b3169687ec829b11d7a252cbf98f11404e
Carl Crowder <carl@akvo.org>
2013-08-14 15:46:28 +0200

    Adding a separate directory owned by the teamcity user to use as the base teamcity directory

--- puppet/modules/teamcity/manifests/init.pp
+++ puppet/modules/teamcity/manifests/init.pp
@@ -3,7 +3,7 @@ class teamcity {
 
     $version = '8.0.2'
     $url = "http://download.jetbrains.com/teamcity/TeamCity-${version}.tar.gz"
-    $dest = "/opt/teamcity-${version}.tar.gz"
+    $dest = "/opt/teamcity/tarballs/teamcity-${version}.tar.gz"
 
     include teamcity::packages
     include teamcity::user
@@ -12,8 +12,16 @@ class teamcity {
         command => "/usr/bin/wget --output-document ${dest} ${url}",
         creates => $dest,
         user    => 'teamcity',
-        cwd     => "/opt/",
-        require => [File['/opt/teamcity'], User['teamcity']],
+        cwd     => "/opt/teamcity/tarballs/",
+        require => [File['/opt/teamcity/tarballs/'], User['teamcity']],
+    }
+
+    file { '/opt/teamcity/tarballs/':
+        ensure  => 'directory',
+        owner   => 'teamcity',
+        group   => 'teamcity',
+        mode    => 750,
+        require => File['/opt/teamcity'],
     }
 
 
