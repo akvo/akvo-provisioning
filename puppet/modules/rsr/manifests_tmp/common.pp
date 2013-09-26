1d4e538a7da4760f31db1e9211b3b9d3b753d23b
Carl Crowder <carl@akvo.org>
2013-09-26 17:33:49 +0200

    Removing temporary files which were useful during refactor

--- puppet/modules/rsr/manifests_tmp/common.pp
+++
@@ -1,8 +0,0 @@
-# This class pulls together the parts which are required by both installed
-# and development versions of RSR
-
-class rsr::common {
-
-
-
-}
--- puppet/modules/rsr/manifests_tmp/installed.pp
+++
@@ -1,6 +0,0 @@
-# this class represents a copy of RSR which is fixed and installed,
-# and which is updated by an automatic deployment process.
-
-class rsr::installed {
-    include rsr::common
-}
