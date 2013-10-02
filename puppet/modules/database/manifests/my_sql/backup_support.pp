48a72e17d362d10fa5ae998e4da00a00c48b29d9
Carl Crowder <carl@akvo.org>
2013-10-02 18:23:10 +0200

    Fixing backup user since a random change randomly broke things (see https://github.com/puppetlabs/puppetlabs-mysql/issues/272)

--- puppet/modules/database/manifests/my_sql/backup_support.pp
+++ puppet/modules/database/manifests/my_sql/backup_support.pp
@@ -5,12 +5,14 @@ class database::my_sql::backup_support {
     $backuppassword = hiera('mysql_backup_password')
 
     mysql_user { "${backupuser}@localhost":
+        ensure        => present,
         password_hash => mysql_password($backuppassword),
         provider      => 'mysql',
         require       => Class['mysql::server'],
     }
 
     mysql_grant { "${backupuser}@localhost":
+        user       => $backupuser,
         privileges => [ 'Select_priv', 'Reload_priv', 'Lock_tables_priv', 'Show_view_priv' ],
         require    => Mysql_user["${backupuser}@localhost"],
     }
