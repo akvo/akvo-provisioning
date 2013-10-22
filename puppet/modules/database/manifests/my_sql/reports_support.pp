f2c7bf1d11d0001da4c443065652065c54d6c990
Carl Crowder <carl@akvo.org>
2013-10-22 13:55:54 +0200

    Fixing the reports user password value

--- puppet/modules/database/manifests/my_sql/reports_support.pp
+++ puppet/modules/database/manifests/my_sql/reports_support.pp
@@ -5,7 +5,7 @@ class database::my_sql::reports_support {
 
     mysql_user { 'reports@localhost':
         ensure        => present,
-        password_hash => mysql_password($mysql_reports_password),
+        password_hash => mysql_password($reports_password),
         provider      => 'mysql',
         require       => Class['mysql::server'],
     }
