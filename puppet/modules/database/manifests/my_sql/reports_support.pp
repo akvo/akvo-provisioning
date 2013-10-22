25436b3f9865d9af090a513ec9be724a3e9c8e95
Carl Crowder <carl@akvo.org>
2013-10-22 15:24:51 +0200

    Reports user can now connect from anywhere to support the reporting tools we use which cannot handle ssh tunnel+socket access

--- puppet/modules/database/manifests/my_sql/reports_support.pp
+++ puppet/modules/database/manifests/my_sql/reports_support.pp
@@ -3,7 +3,7 @@ class database::my_sql::reports_support {
 
     $reports_password = hiera('mysql_reports_password')
 
-    mysql_user { 'reports@localhost':
+    mysql_user { 'reports@%':
         ensure        => present,
         password_hash => mysql_password($reports_password),
         provider      => 'mysql',
