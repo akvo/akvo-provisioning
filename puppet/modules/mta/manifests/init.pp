6e2174db42fdb87e3306aae9000cd7a88c37159b
Carl Crowder <carl@akvo.org>
2013-10-31 14:22:41 +0100

    Fixing permissions so all users can read main.cf (and therefore can send mail)

--- puppet/modules/mta/manifests/init.pp
+++ puppet/modules/mta/manifests/init.pp
@@ -8,7 +8,7 @@ class mta {
     $base_domain = hiera('base_domain')
     file { '/etc/postfix/main.cf':
         ensure  => present,
-        mode    => '0440',
+        mode    => '0444',
         owner   => 'root',
         content => template('mta/main.cf.erb'),
         require => Package['postfix']
