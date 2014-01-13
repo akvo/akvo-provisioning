7bacdade62f925c471969053f90d92d1e40d2985
Carl Crowder <carl@akvo.org>
2014-01-13 11:53:26 +0100

    Oops, fixing the ssh key for Stellan

--- puppet/modules/users/manifests/init.pp
+++ puppet/modules/users/manifests/init.pp
@@ -28,7 +28,7 @@ class users {
     users::basic { 'stellan':
         roles    => ['developer'],
         allow    => ['munin'],
-        ssh_key  => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAAAgQDE+s/CSGkvynr/7nWe7/9Vkv2Nv6m84HTruEo9yN5vpvDp5j6XpsNklq9NgLYQs8/C46iKvbFTF/apIauHCr1lC27PI0m2n5coUNbNGoJZMhWoq4LmHdbhu4vhDgz+Vqx0hbQrNHiG7XIhsaqFnCJua8faKJUn6RKTantQr5aQDQ==',
+        ssh_key  => 'AAAAB3NzaC1yc2EAAAADAQABAAAAgQDE+s/CSGkvynr/7nWe7/9Vkv2Nv6m84HTruEo9yN5vpvDp5j6XpsNklq9NgLYQs8/C46iKvbFTF/apIauHCr1lC27PI0m2n5coUNbNGoJZMhWoq4LmHdbhu4vhDgz+Vqx0hbQrNHiG7XIhsaqFnCJua8faKJUn6RKTantQr5aQDQ==',
     }
 
     users::basic { 'loic':
