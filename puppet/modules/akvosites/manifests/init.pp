ec2c4755f439ad068e55014d18049ed1e838b075
Carl Crowder <carl@akvo.org>
2014-02-14 14:30:36 +0100

    [refs #48] Adding cronjobs to update akvo sites

--- puppet/modules/akvosites/manifests/init.pp
+++ puppet/modules/akvosites/manifests/init.pp
@@ -25,13 +25,59 @@ class akvosites {
         path => "${app_path}/code"
     }
 
-    file { "/var/akvo/akvosites/code/wp-config.php":
+    file { "${app_path}/code/wp-config.php":
         ensure  => present,
         owner   => 'akvosites',
         group   => 'akvosites',
         mode    => '0444',
         content => template('akvosites/wp-config.php.erb'),
-        require => File["/var/akvo/akvosites/code"]
+        require => File["${app_path}/code"]
+    }
+
+    file { "${app_path}/scripts":
+        ensure  => directory,
+        owner   => 'akvosites',
+        group   => 'akvosites',
+        mode    => '0444',
+        require => File[$app_path]
+    }
+
+    file { "${app_path}/scripts/sync_sites_updates.sh":
+        ensure  => present,
+        owner   => 'akvosites',
+        group   => 'akvosites',
+        mode    => '0544',
+        content => template('akvosites/sync_sites_updates.sh.erb'),
+        require => File["${app_path}/scripts"]
+    }
+
+    cron { 'sync_sites_updates':
+        ensure  => present,
+        user    => 'akvosites',
+        weekday => '*',
+        hour    => '*/6',
+        minute  => '15',
+        command => "${app_path}/scripts/sync_sites_updates.sh",
+        require => File["${app_path}/scripts/sync_sites_updates.sh"]
+    }
+
+    file { "${app_path}/scripts/update_akvo_sites_data.sh":
+        ensure  => present,
+        owner   => 'akvosites',
+        group   => 'akvosites',
+        mode    => '0544',
+        content => template('akvosites/update_akvo_sites_data.sh.erb'),
+        require => File["${app_path}/scripts"]
+    }
+
+    cron { 'update_akvo_sites_data':
+        ensure  => present,
+        user    => 'akvosites',
+        weekday => '*',
+        hour    => '*/3',
+        minute  => '5',
+        command => "${app_path}/scripts/update_akvo_sites_data.sh",
+        require => File["${app_path}/scripts/update_akvo_sites_data.sh"]
     }
 
     database::my_sql::db { 'akvosites':
