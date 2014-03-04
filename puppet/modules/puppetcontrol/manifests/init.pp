99ef607890097964651fc5726c61b0accf6a3698
Carl Crowder <carl@akvo.org>
2014-03-04 11:34:32 +0100

    [refs #52] I have no idea why the puppet ssh key was removed...

--- puppet/modules/puppetcontrol/manifests/init.pp
+++ puppet/modules/puppetcontrol/manifests/init.pp
@@ -42,6 +42,13 @@ class puppetcontrol {
         require => File['/puppet/.ssh']
     }
 
+    ssh_authorized_key { "puppet_key":
+        ensure  => present,
+        key     => hiera('puppet_public_key'),
+        type    => 'ssh-rsa',
+        user    => 'puppet',
+        require => File['/puppet/.ssh/authorized_keys']
+    }
 
     # add some additional helper scripts
     file { '/puppet/bin/':
