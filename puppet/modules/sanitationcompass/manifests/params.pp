13ada97fe61056cc8930f3dc8e02fc0a130e75f8
Carl Crowder <carl@akvo.org>
2014-05-07 12:09:35 +0200

    [refs #73] Using environment variables to configure the sanitationcompass service

--- puppet/modules/sanitationcompass/manifests/params.pp
+++ puppet/modules/sanitationcompass/manifests/params.pp
@@ -5,6 +5,16 @@ class sanitationcompass::params {
     $approot = '/var/akvo/sanitationcompass'
     $port = 8100
 
+    $database_name = 'sanitcompass'
     $database_password = hiera('sanitationcompass_database_password')
+
+    $base_domain = hiera('base_domain')
+
+    $env_vars = {
+        'PYTHONPATH'    => "${approot}/code/dsp",
+        'DATABASE_URL'  => "mysql://${database_name}:${database_password}@mysql/${database_name}",
+        'SECRET_KEY'    => hiera('sanitationcompass_secret_key'),
+        'ALLOWED_HOSTS' => "sanitationcompass.info,sanitationcompass.${base_domain}",
+    }
 
 }
