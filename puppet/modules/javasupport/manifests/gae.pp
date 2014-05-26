93abba514f804f9cc66289ff2db0a1047bf6a4c3
Carl Crowder <carl@akvo.org>
2014-05-26 15:31:59 +0200

    [refs #84] Fixing the download URL for the 1.8.x series

--- puppet/modules/javasupport/manifests/gae.pp
+++ puppet/modules/javasupport/manifests/gae.pp
@@ -1,7 +1,8 @@
 class javasupport::gae {
 
-    $gae_version = 'appengine-java-sdk-1.8.9.zip'
-    $gae_sdk = "https://storage.googleapis.com/appengine-sdks/featured/${gae_version}"
+    $gae_version = '1.8.9'
+    $gae_file = "appengine-java-sdk-${gae_version}.zip"
+    $gae_sdk = "https://console.developers.google.com/m/cloudstorage/b/appengine-sdks/o/deprecated/189/${gae_file}"
 
     file { '/opt/gae':
         ensure => directory,
@@ -10,7 +11,7 @@ class javasupport::gae {
     }
 
     exec { 'install_gae_sdk':
-        command => "/usr/bin/wget ${gae_sdk} && /usr/bin/unzip ${gae_version}",
+        command => "/usr/bin/wget ${gae_sdk} && /usr/bin/unzip ${gae_file}",
         cwd     => '/opt/gae',
         creates => "/opt/gae/${gae_version}",
         require => File['/opt/gae']
