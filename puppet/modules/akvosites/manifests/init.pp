29ba72a38ea9080e9ce3bd42ef4678491e941b16
Carl Crowder <carl@akvo.org>
2014-08-04 17:08:56 +0200

    [refs #88] Using a specified default domain for akvosites if one is available in order to get around the issue where the environment-relative domain such as akvosites.test.akvo-ops.org is not configured in the WordPress multisite settings

--- puppet/modules/akvosites/manifests/init.pp
+++ puppet/modules/akvosites/manifests/init.pp
@@ -5,7 +5,7 @@ class akvosites {
     $db_password = hiera('akvosites_database_password')
 
     $base_domain = hiera('base_domain')
-    $default_domain = "akvosites.${base_domain}"
+    $default_domain = hiera('akvosites_default_domain', "akvosites.${base_domain}")
     $akvosites_hostnames = hiera('akvosites_hostnames')
     $all_hostnames = concat($akvosites_hostnames, [$default_domain])
     $app_path = '/var/akvo/akvosites'
