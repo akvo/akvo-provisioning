07c5ffe7ca061d5c0838a84d28b9d2999a551ff9
carlio <git@carlcrowder.com>
2014-10-15 17:05:20 +0200

    [refs #112] The homepage deploymnet process needs to reload php-fpm to ensure the new code is used

--- puppet/modules/homepage/manifests/service.pp
+++ puppet/modules/homepage/manifests/service.pp
@@ -1,4 +1,15 @@
 
 class homepage::service {
 
+    # we want the homepage user to be able to restart the process
+    sudo::allow_command { "homepage-stop-service":
+        user    => 'homepage',
+        command => '/etc/init.d/php5-fpm stop'
+    }
+
+    sudo::allow_command { "homepage-start-service":
+        user    => 'homepage',
+        command => '/etc/init.d/php5-fpm start'
+    }
+
 }
