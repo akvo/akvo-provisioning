bf66aa44b48209a7b4986b3eb4d925a5058b878d
carlio <git@carlcrowder.com>
2014-11-25 13:45:09 +0100

    [refs #124] Removing the rsr_reload job from RSR development boxes

--- puppet/modules/rsr/manifests/development.pp
+++ puppet/modules/rsr/manifests/development.pp
@@ -5,23 +5,6 @@ class rsr::development ( $enabled = false ) {
         include rsr::params
         $approot = $rsr::params::approot
 
-        # add the watcher script
-        file { "${approot}/observe_rsr.sh":
-            ensure => 'present',
-            source => 'puppet:///modules/rsr/observe_rsr.sh',
-            owner  => 'rsr',
-            mode   => '0744',
-            notify => Class['Supervisord::Update'],
-        }
-
-        # start the watchdog process to keep an eye for changes
-        supervisord::service { "rsr_reload":
-            user      => 'rsr',
-            command   => "/bin/bash ${approot}/observe_rsr.sh",
-            directory => $approot,
-            require   => File["${approot}/observe_rsr.sh"],
-        }
-
         # we need unzip to handle the dev db
         package { 'unzip':
             ensure => installed
