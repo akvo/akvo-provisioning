71b740c470fd0760013d8dee31e0a1b69fe571c4
Stellan Lagerstrom <stellanl@akvo.org>
2015-03-10 18:37:59 +0100

    [#132]Put db connection file in right place

--- puppet/modules/reporter/manifests/install.pp
+++ puppet/modules/reporter/manifests/install.pp
@@ -93,7 +93,7 @@ class reporter::install {
         cwd     => "${approot}",
         creates => "${approot}/.installed",
         require => [File["${approot}/install_reportserver.sh"],
-                    File["${approot}/persistence.xml"],
+                    File["${approot}/META-INF/persistence.xml"],
                     File["${approot}/rsbirt.jar.patch2"],
                     File["${approot}/rsbase.jar.patch2"],
                     Package['unzip']]
