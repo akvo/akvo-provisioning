b7386a954dea59d54913b46fe78d92d1c64f6348
Oriol Fitó <oriol@akvo.org>
2015-03-20 11:14:19 +0100

    [#135] Addding self-signed certificate for localdev env

--- puppet/modules/nginx/manifests/proxy.pp
+++ puppet/modules/nginx/manifests/proxy.pp
@@ -62,7 +62,7 @@ define nginx::proxy( $server_name = undef,
   if $ssl {
       file { $ssl_key:
           ensure  => present,
-          source  => $ssl_key_source,
+          content => $ssl_key_source,
           owner   => 'root',
           group   => 'root',
           mode    => '0444',
@@ -72,7 +72,7 @@ define nginx::proxy( $server_name = undef,
 
       file { $ssl_crt:
           ensure  => present,
-          source  => $ssl_cert_source,
+          content => $ssl_cert_source,
           owner   => 'root',
           group   => 'root',
           mode    => '0444',
