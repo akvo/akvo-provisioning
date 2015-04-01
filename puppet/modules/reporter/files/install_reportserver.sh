bd09348dc89a0b764c8f3ff618839accb993546e
Stellan Lagerstrom <stellanl@akvo.org>
2015-04-01 15:36:47 +0200

    [#132]Test with a patched reportserver dist

--- puppet/modules/reporter/files/install_reportserver.sh
+++ puppet/modules/reporter/files/install_reportserver.sh
@@ -2,7 +2,8 @@
 set -e
 
 #fetch release zipfile (Approx 200MB; too big for GitHub)
-RELEASE="RS2.2.1-5602-reportserver.zip"
+#RELEASE="RS2.2.1-5602-reportserver.zip"
+RELEASE=RS2.2.0-5654-2015-04-01-11-00-17-reportserver.zip
 #next version: RELEASE="RS2.2.2-5639-reportserver.zip"
 wget http://files.support.akvo-ops.org/reportserver/$RELEASE
 
@@ -10,12 +11,12 @@ wget http://files.support.akvo-ops.org/reportserver/$RELEASE
 unzip $RELEASE
 
 #overwrite patched jar files
-cp rsbirt.jar.patch2 WEB-INF/lib/rsbirt.jar
-cp rsbase.jar.patch2 WEB-INF/lib/rsbase.jar
-cp rssaiku.jar.patch3 WEB-INF/lib/rssaiku.jar
+#cp rsbirt.jar.patch2 WEB-INF/lib/rsbirt.jar
+#cp rsbase.jar.patch2 WEB-INF/lib/rsbase.jar
+#cp rssaiku.jar.patch3 WEB-INF/lib/rssaiku.jar
 
 #save some disk space
-rm $RELEASE
+#rm $RELEASE
 
 #if we make it this far, we succeeded, so prevent another run
 touch .installed
