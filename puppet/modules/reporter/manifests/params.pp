decab248002d0b45a30f8053e41c73d69217ee99
Stellan Lagerstrom <stellanl@akvo.org>
2015-04-24 01:37:10 +0200

    [#152]Add parameters and template for custom crypto config file

--- puppet/modules/reporter/manifests/params.pp
+++ puppet/modules/reporter/manifests/params.pp
@@ -8,6 +8,10 @@ class reporter::params {
     $db_name = 'reportserver'
     $db_password = hiera('reporter_database_password')
 
+    $rs_crypto_pbe_salt = hiera('rs_crypto_pbe_salt')
+    $rs_crypto_pbe_passphrase = hiera('rs_crypto_pbe_passphrase')
+    $rs_crypto_hmac_passphrase = hiera('rs_crypto_hmac_passphrase')
+
     $approot = '/var/lib/tomcat7/webapps/reportserver'
     $tomcatconf = '/var/lib/tomcat7/conf/server.xml'
 
