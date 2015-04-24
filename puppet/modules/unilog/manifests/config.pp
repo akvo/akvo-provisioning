d2162cf779b19d90316aa1177102331dc09dd988
Oriol Fitó <oriol@akvo.org>
2015-04-24 15:57:10 +0200

    [akvo/akvo-infrastructure#88] Basic unilog configuration, installation and service mgmt

--- puppet/modules/unilog/manifests/config.pp
+++ puppet/modules/unilog/manifests/config.pp
@@ -1,3 +1,21 @@
-class unilog::config {
+class unilog::config inherits unilog::params {
+
+    database::psql::db { $appname:
+        psql_name  => $postgres_name,
+        password   => $database_password
+    }
+
+    # we want a service address
+    named::service_location { $appname:
+        ip => hiera('external_ip')
+    }
+
+    # nginx sits in front of unilog
+    $base_domain = hiera('base_domain')
+    nginx::proxy { "unilog.${base_domain}":
+        proxy_url  => "http://localhost:${port}",
+        access_log => "${logdir}/unilog-nginx-access.log",
+        error_log  => "${logdir}/unilog-nginx-error.log",
+    }
 
 }
