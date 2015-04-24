2d2ed2a22a217acaa47bd8d51fba74e1c58aefc8
Oriol Fitó <oriol@akvo.org>
2015-04-24 15:54:03 +0200

    [akvo/akvo-infrastructure#88] Adding unilog puppet module parameters

--- puppet/modules/unilog/manifests/params.pp
+++ puppet/modules/unilog/manifests/params.pp
@@ -1,3 +1,22 @@
 class unilog::params {
 
+    $username            = 'unilog'
+    $appname             = 'unilog'
+    $approot             = "/var/akvo/${appname}"
+    $workdir             = "${approot}/code"
+    $logdir              = "${approot}/logs"
+    $port                = '3030'
+    $env_vars            = {
+        'HOME' => $approot
+    }
+
+    $remote_api_email    = 'reports@akvoflow.org'
+    $remote_api_password = hiera('unilog_remote_api_password')
+
+    $flow_server_config  = "${approot}/akvo-flow-server-config"
+    $flow_data_schema    = "${approot}/akvo-core-services/flow-data-schema/schema/event.json"
+
+    $postgres_name       = hiera('unilog_psql_name')
+    $database_password   = hiera('unilog_database_password')
+
 }
