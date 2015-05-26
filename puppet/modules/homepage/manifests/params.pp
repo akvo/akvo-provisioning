c967d39d4ef6e4d8c8c579c839fffaee5eaee0a7
Oriol Fitó <oriol@akvo.org>
2015-05-26 11:19:19 +0200

    [akvo/akvo-web#562] Removing useless default hostname

--- puppet/modules/homepage/manifests/params.pp
+++ puppet/modules/homepage/manifests/params.pp
@@ -7,10 +7,7 @@ class homepage::params {
     $mysql_name = hiera('homepage_wordpress_database_mysql_name', 'mysql')
     $db_password = hiera('homepage_wordpress_database_password')
 
-    $specified_hostnames = hiera('homepage_hostnames')
-    $base_domain = hiera('base_domain')
-    $default_hostname = ["homepage.${base_domain}"]
-    $homepage_hostnames = concat($default_hostname, $specified_hostnames)
+    $homepage_hostnames = hiera('homepage_hostnames')
     $homepage_url = hiera('homepage_url')
     $rsr_domain = hiera('rsr_main_domain') # for legacy redirects
     $pool_port = 9010
