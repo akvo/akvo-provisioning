ff01fcff788bc8b8ca2ba12f09070be1e6754fbf
Oriol Fitó <oriol@akvo.org>
2015-06-10 16:23:05 +0200

    Revert "[akvo/akvo-web#562] Restoring base_domain parameter"
    This reverts commit c31748cfe20c4803bb655f45835d956809a6f558.

--- puppet/modules/homepage/manifests/params.pp
+++ puppet/modules/homepage/manifests/params.pp
@@ -7,7 +7,6 @@ class homepage::params {
     $mysql_name = hiera('homepage_wordpress_database_mysql_name', 'mysql')
     $db_password = hiera('homepage_wordpress_database_password')
 
-    $base_domain = hiera('base_domain')
     $homepage_hostnames = hiera('homepage_hostnames')
     $homepage_url = hiera('homepage_url')
     $rsr_domain = hiera('rsr_main_domain') # for legacy redirects
