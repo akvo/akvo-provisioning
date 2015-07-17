6962a0330127e4532f06bb2316cfeceaefb929da
Oriol Fitó <oriol@akvo.org>
2015-07-17 16:34:34 +0200

    [akvo/akvo-infrastructure#88] Allowing psql connections only from specified clients

--- puppet/modules/unilog/manifests/params.pp
+++ puppet/modules/unilog/manifests/params.pp
@@ -21,6 +21,7 @@ class unilog::params {
     $postgres_name       = hiera('psql_name')
     $postgres_host       = "${postgres_name}.${base_domain}"
     $postgres_port       = hiera('psql_port', '5432')
+    $postgres_clients    = hiera_hash('unilog_psql_clients')
     $database_password   = hiera('unilog_database_password')
 
 }
