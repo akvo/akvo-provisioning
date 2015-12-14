57605a43df60ea149e13a0444da6309172f1c6ca
Paul Burt <paul.burt@gmail.com>
2015-12-14 17:36:49 +0000

    [#240] Added postgresql_password configuration option to postgresql db module

--- puppet/modules/database/manifests/psql/server.pp
+++ puppet/modules/database/manifests/psql/server.pp
@@ -34,6 +34,7 @@ class database::psql::server {
        ip_mask_deny_postgres_user => '0.0.0.0/32',
        ip_mask_allow_all_users => '0.0.0.0/0',
        listen_addresses => $external_ip,
+       postgresql_password => hiera('postgresql_password') 
     }
 
     # let everyone know where we are
