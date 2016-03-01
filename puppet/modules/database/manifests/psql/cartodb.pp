2e86d5e35b0a06be4560a0ca86ea648387290bdc
Oriol Fitó <oriol@akvo.org>
2016-03-01 10:00:29 +0100

    [#275] Declaring postgres_clients variable on cartodb manifest

--- puppet/modules/database/manifests/psql/cartodb.pp
+++ puppet/modules/database/manifests/psql/cartodb.pp
@@ -8,6 +8,7 @@ class database::psql::cartodb {
     $psql_version = hiera('psql_version', '9.3')
     $psql_password = hiera('psql_password', 'password')
     $data_dir = hiera('psql_data_directory', undef)
+    $postgres_clients = hiera_hash('cartodb_psql_clients')
 
     # grant all permissions needed to 'postgres' user
     postgresql::server::role { 'postgres':
