f342ac44a8d3508525e1cf7a3e2e21d79b0bbf8d
Oriol Fitó <orifito@gmail.com>
2017-08-01 17:57:53 +0200

    Removing Oliver user

--- puppet/modules/users/manifests/init.pp
+++ puppet/modules/users/manifests/init.pp
@@ -3,13 +3,6 @@ class users {
     include users::groups
     include users::root
 
-    users::basic { 'oliver':
-        roles    => ['ops', 'www-edit', 'reporting'],
-        allow    => ['munin'],
-        ssh_key  => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQC0bMRyG8EspxOf7p2+sqWsTDq3RX6VnO65MWrp1SrCQhG6tMAsKeza82J2UuCN+GFTEO4yyO05s942UZ+5vgV+5CZ1Z6tlI7SFvovZM+fNSJn/BzQC8s1nAsVFMoAFKNNcLb7LS0rUhZ+RmDaCxIoH2TcbhxBVbynR41kbQfL1+oWnp3xmEeb/4/NKMaVYh3R/cxSe1GLlr19E/btywxzF4CGn5fNjkherrg7Bv7Mc7PmOezE7uwfbidGF0alYNOSPCKcI4t+kookD5XzP3sKPqVWCXjwSCrnuR/ViWKG1TxZtvIdKvRJwA7X7Y5eL1c9UruWtZio+Tyqa4u9obn9x',
-        htpasswd => '$apr1$WdYFnpfb$nE14MA.Yra5q6K5MILDOO.'
-    }
-
     users::basic { 'oriol':
         roles    => ['ops', 'www-edit', 'reporting'],
         allow    => ['munin'],
@@ -137,6 +130,7 @@ class users {
     users::remove_user { ['dan']: }
     users::remove_user { ['carl']: }
     users::remove_user { ['adrian']: }
+    users::remove_user { ['oliver']: }
 
     Class['Users::Groups'] -> Users::Basic<||>
 }
